09bc4d1ef450003c9e6eb0225906f80e210e39e4
Ramond Falcão <ramondfalcao@gmail.com>
2021-06-10 20:34:56 -0300

    atualização

--- programacaoImperativa/exercicioArrays.js
+++ programacaoImperativa/exercicioArrays.js
@@ -3,7 +3,7 @@ let filmes = ["Star wars","Clube da luta", "O poderoso chefão", "Top gun","Inte
 let cartoons = ["Toy story", "Procurando Nemo","Kung-fu Panda","Wally","Fortnite"]
 
 
-// console.log(filmes[1])
+console.log(filmes[1])
 
 function adcionar(){
     filmes.push(...cartoons);
