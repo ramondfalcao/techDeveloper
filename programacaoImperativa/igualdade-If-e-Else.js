f14f03634f5bd4134b81d2927db279bd085b54e2
Ramond Falcão <ramondfalcao@gmail.com>
2021-06-10 17:56:36 -0300

    exercicios

---
+++ programacaoImperativa/igualdade-If-e-Else.js
@@ -0,0 +1,8 @@
+let linguagem = "Java"
+
+if (linguagem == "javascript"){
+    console.log('Estou aprendendo :)')
+}  else {
+    console.log('Aprenderei mais tarde')
+}
+
---
+++ programacaoImperativa/meuPrimeiroIf.js
@@ -0,0 +1,9 @@
+let status = true
+
+if (status == true) {
+    console.log('O valor é true - verdadeiro');    
+}   else {
+    console.log('O valor é false - falso');
+}
+
+// Demorei pra entender que o igual deve ser assim "=="   
