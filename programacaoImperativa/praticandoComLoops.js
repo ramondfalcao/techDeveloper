9894ea1158d5135c5584ad443f4a19db8af7a962
Ramond Falcão <ramondfalcao@gmail.com>
2021-06-13 15:10:50 -0300

    exercicos usando loops

---
+++ programacaoImperativa/praticandoComLoops.js
@@ -0,0 +1,8 @@
+ for (let i = 1; i<=5; i++){
+     console.log("Olá Mundo")  //Repetir como um papagaio 
+ }
+
+ for (let i = 1; i<=10; i+=2){  //Contando numeros impares
+     console.log(i)
+ }
+
---
+++ programacaoImperativa/tabuadaUsandoFor.js
@@ -0,0 +1,5 @@
+let valor = 8;
+
+for(let i = 1; i<=10; i++){
+     console.log(valor + " x " + i + " = " + (valor * i))  
+}
