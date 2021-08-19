dfc1f46c98666f514f76e1705430170d14db48b4
Ramond Falcão <ramondfalcao@gmail.com>
2021-08-19 16:18:41 -0300

    exercicos em java

--- projetosJava/src/impressora/Impressora.java
+++ projetosJava/src/impressora/Impressora.java
@@ -1,2 +1,42 @@
-package impressora;public class Impressora {
+package impressora;
+
+import java.util.Date;
+
+public class Impressora {
+
+    private String modelo;
+    private String tipoConexao;
+    private Date dataFabricacao;
+    private int folhasDisponiveis;
+
+    public Impressora(String modelo, String tipoConexao, Date dataFabricacao){
+        this.modelo = modelo;
+        this.tipoConexao = tipoConexao;
+        this.dataFabricacao = dataFabricacao;
+        this.folhasDisponiveis = 0;
+    }
+
+    public void setFolhasDisponiveis(int folhasDisponiveis) {
+        this.folhasDisponiveis = folhasDisponiveis;
+    }
+
+    private boolean temPapel(){
+        boolean estado;
+        if(this.folhasDisponiveis == 0)
+            estado = false;
+        else
+            estado = true;
+
+        return estado;
+    }
+
+
+    public void imprimir(String texto){
+        if(temPapel())
+            System.out.println(texto);
+        else
+            System.out.println("Sem papel!");
+    }
 }
+
+
--- projetosJava/src/impressora/Principal.java
+++ projetosJava/src/impressora/Principal.java
@@ -1,4 +1,14 @@
 package impressora;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Principal {
+    public static void main(String[] args) {
+        Date data = new Date("20/08/1990") ;
+        Impressora impressora = new Impressora("Epson", "wifi", data);
+        impressora.setFolhasDisponiveis(100);
+        impressora.imprimir("OlÃ¡ Mundo");
+
+    }
 }
